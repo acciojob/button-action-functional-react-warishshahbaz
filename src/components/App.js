4f7db31f29b2267d646e5370571c2c841a0857e2
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-08 15:38:09 +0530

    enhanced

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [buttonClicked, setButtonClicked] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* Button with an ID of 'click' */}
+      <button id="click" onClick={() => setButtonClicked(true)}>
+        Click me
+      </button>
+
+      {/* Conditional rendering of the paragraph tag */}
+      {buttonClicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
